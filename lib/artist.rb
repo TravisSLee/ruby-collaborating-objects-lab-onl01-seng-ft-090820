ebfacc27b8cadc33d094cb5adda71cecc768d5d5
Travis Lee <travisslee223@gmail.com>
2020-09-28 19:21:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,13 +6,18 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+<<<<<<< HEAD
     save
+=======
+    @all << self
+>>>>>>> 4978db4d1997df3a90b53f74dbc50c42c0db2047
   end
 
   def add_song(song)
     @songs << song
   end
 
+<<<<<<< HEAD
   def self.all
     @@all 
   end
@@ -36,6 +41,24 @@ class Artist
       self.find(name)
     else
       self.create(name) 
+=======
+  def save
+    @@all << self 
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def self.find_or_create_by_name(artist_name)
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new(artist_name)
+      new_artist.save
+      new_artist
+>>>>>>> 4978db4d1997df3a90b53f74dbc50c42c0db2047
     end
   end
 
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,11 @@ class MP3Importer
   end
 
   def files
+<<<<<<< HEAD
     Dir[@path+"/*.mp3"].map { |file| file.split("/").last }
+=======
+    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+>>>>>>> 4978db4d1997df3a90b53f74dbc50c42c0db2047
   end
 
   def import
